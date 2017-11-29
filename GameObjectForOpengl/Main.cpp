04b6a53699b509a5484f8575a5ed1d0c02574302
Minstreams <minstreams@qq.com>
2017-11-29 14:23:06 +0800

    更新测试场景

--- GameObjectForOpengl/Main.cpp
+++ GameObjectForOpengl/Main.cpp
@@ -1,57 +1,61 @@
 #include "OpenglIndex.h"
 #include "TransformIndex.h"
-#include "UnityIndex.h"
+#include "ModelIndex.h"
+
+#include "Shaders.h"
+#include "Models.h"
 #include "GameObjects.h"
 #include "Components.h"
 
 void SetScene() {
 
-	mainScene.SetCamera(Vector3(0, 0, -300), Quaternion::EulerY(180));
-	//mainScene.camera.AddComponent(new MouseRotater(0.05f, 0.05f));
-	//mainScene.camera.AddComponent(new PressLToShowPosition());
+	mainScene.SetCamera(Vector3(0, 1, 3), Quaternion::identity);
 
-	GameObject *sun = mainScene.AddGameObject(new Moon("t1", 8, Vector3(0, 0, -200), Quaternion::EulerY(180)));
+	GameObject *sun = AddGameObject(new GameObject("t1", Vector3(0, 0, -200), Quaternion::EulerY(180)));
+	AddComponent(new RigidBody(30, 0.1));
+	AddComponent(new MouseRotater(0.05f, 0.05f));
+	AddComponent(new FPSRigidBodyMover(20));
+	AddComponent(new SphereCollider(3, Vector3(0,-10,0), 1));
+	AddComponent(new PressLToShowPosition());
+
+	AddChild(new TestModel("t1", Vector3(0, -10, 0), Quaternion::EulerY(180)));
+	AddComponent(new MouseRotater(0, 0.05f));
 	mainScene.camera.AddComponent(new Tracker(&(sun->transform), 0.9, true, false));
-	sun->AddComponent(new RigidBody(10));
-	sun->AddComponent(new MouseRotater(0.05f, 0.05f));
-	sun->AddComponent(new FPSRigidBodyMover(20));
-	sun->AddComponent(new SphereCollider(8, Vector3::zero, 1));
 
+	sun = sun->AddChild(new GameObject("camPos", Vector3(0, 0, 30), Quaternion::EulerY(180)));
 
-	sun = sun->AddChild(new GameObject("camPos", Vector3(0, 0, 100), Quaternion::EulerY(180)));
-	sun->AddComponent(new PressLToShowPosition());
 	mainScene.camera.AddComponent(new Tracker(&(sun->transform), 0.9));
 
-	GameObject *t = mainScene.AddGameObject(new Sun("t2", 6, Vector3(10, 0, -100), Quaternion::identity));
-	t->AddComponent(new RigidBody());
-	t->AddComponent(new SphereCollider(6));
+	AddGameObject(new Sun("t2", 6, Vector3(10, 0, -100), Quaternion::identity));
+	AddComponent(new RigidBody());
+	AddComponent(new SphereCollider(6));
 
-	GameObject *g = mainScene.AddGameObject(new Sun("Sun!"));
-	g->AddComponent(new AutoRotate(Vector3(0, 12, 0)));//公转速度
-	g->AddComponent(new SphereCollider(50, Vector3::zero, 1));
-	g->AddComponent(new LightComponent());
-
-
-	g = g->AddChild(new GameObject("Sun's Son", Vector3(80, 0, 0), Quaternion::identity));
-	g->AddComponent(new AutoRotate(Vector3(0, -5, 0)));//抵消公转速度
+	AddGameObject(new TestModel("Sun!"));
+	AddComponent(new AutoRotate(Vector3(0, 12, 0)));//公转速度
+	AddComponent(new SphereCollider(4, Vector3::zero, 1));
+	AddComponent(new LightComponent());
 
 
-	g = g->AddChild(new GameObject("Earth's parent", Vector3(0, 0, 0), Quaternion::Euler(Vector3(0, 0, 30))));
-	g->AddComponent(new AutoRotate(Vector3(0, 10, 0)));//自转速度
+	AddChild(new GameObject("Sun's Son", Vector3(80, 0, 0), Quaternion::identity));
+	AddComponent(new AutoRotate(Vector3(0, -5, 0)));//抵消公转速度
 
 
-	GameObject *earth = g->AddChild(new Earth("Earth"));
-	g->AddComponent(new SphereCollider(5));
-	//earth->AddComponent(new PressLToShowPosition());
+	AddChild(new GameObject("Earth's parent", Vector3(0, 0, 0), Quaternion::Euler(Vector3(0, 0, 30))));
+	AddComponent(new AutoRotate(Vector3(0, 10, 0)));//自转速度
 
-	g->AddComponent(new AutoRotate(Vector3(0, 60, 0)));//月球公转速度
-	g = g->AddChild(new Moon("Moon", 2, Vector3(20, 0, 0), Quaternion::Euler(Vector3(0, 0, 0))));
-	//g->AddComponent(new PressLToShowPosition());
+
+	GameObject *earth = AddChild(new Earth("Earth"));
+	AddComponent(new SphereCollider(5));
+	//AddComponent(new PressLToShowPosition());
+	AddComponent(new AutoRotate(Vector3(0, 60, 0)));//月球公转速度
+
+	AddChild(new Moon("Moon", 2, Vector3(20, 0, 0), Quaternion::Euler(Vector3(0, 0, 0))));
+	//AddComponent(new PressLToShowPosition());
 }
 
 void SetLight() {
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHTING);
+	//glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHTING);
 }
 
 int main(int argc, char* argv[])
