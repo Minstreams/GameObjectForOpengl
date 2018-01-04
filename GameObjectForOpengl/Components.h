5b21fccb311794df6512478e7fee19deaf019562
Minstreams <minstreams@qq.com>
2018-01-04 08:09:27 +0800

    添加Gravity组件
    就是让物体收到重力的组件啦

--- GameObjectForOpengl/Components.h
+++ GameObjectForOpengl/Components.h
@@ -6,3 +6,5 @@
 #include "LightComponent.h"
 #include "PressLToShowPosition.h"
 #include "Tracker.h"
+
+#include "Gravity.h"
---
+++ GameObjectForOpengl/Gravity.h
@@ -0,0 +1,19 @@
+#pragma once
+class Gravity : public MonoBehavour {
+public:
+	double gravity;
+	RigidBody *rid = NULL;
+	Gravity(double gravity) :gravity(gravity) {}
+	void Awake() override {
+		if (gameObject->rigidBody == NULL) {
+			ShowWarnMessage("There is no rigidBody!!\n没有刚体组件！！", "来自Gravity组件");
+			gameObject->DestroyComponent(this);
+		}
+		else {
+			rid = gameObject->rigidBody;
+		}
+	}
+	void Update() override {
+		rid->velocity.y -= gravity * deltaTime;
+	}
+};
